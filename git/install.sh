bbf78a8b13c52d76bc5870a10dedc00c1bdd7c46
Guilherme Lazzaris <elysium@MacBook.local>
2017-03-26 23:15:56 -0300

    Add gitignore_global file and symbolic link for home folder

--- git/install.sh
+++ git/install.sh
@@ -7,3 +7,13 @@ fi
 
 ln -s $HOME/.dotfiles/git/.gitconfig ~/.gitconfig
 echo "~/.gitconfig [installed]"
+
+# Check if exist gitignore_global file
+if [ -e "$HOME/.gitignore_global" ]; then
+  rm -f $HOME/.gitignore_global
+fi
+
+ln -s $HOME/.dotfiles/git/.gitignore_global ~/.gitignore_global
+echo "~/.gitignore_global [installed]"
+
+git config --global core.excludesfile ~/.gitignore_global
